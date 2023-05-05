95e5956714218280d6dff4a2b4d1e8d0b05ef7de
Ulisses Pereira da Silva Neto <upneto@gmail.com>
2023-05-05 00:34:25 -0300

    Versao implantação

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
   base: URL_BASE,
   api: {
     login: URL_BASE + '/login',
-    categoria: URL_BASE + '/api/categoria',
-    cliente: URL_BASE + '/api/cliente',
-    produto: URL_BASE + '/api/produto',
-    reclamacao: URL_BASE + '/api/reclamacao',
-    anexo: URL_BASE + '/api/anexo'
+    categoria: URL_BASE + '/categoria',
+    cliente: URL_BASE + '/cliente',
+    produto: URL_BASE + '/produto',
+    reclamacao: URL_BASE + '/reclamacao',
+    anexo: URL_BASE + '/anexo'
   }
 }
